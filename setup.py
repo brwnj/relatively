c96484d693deea71c3a6d0374e0871e80aa0fee9
Joe Brown <brwnjm@gmail.com>
2018-08-07 10:55:41 -0700

    adds requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
-setup(name='relatively',
-      version='1.0.2',
-      description='Abundance figure with absolute and relative abundances across a hierarchy',
-      url='https://github.com/brwnj/relatively',
-      author='Joe Brown',
-      author_email='brown@pm.me',
-      license='MIT',
-      packages=['relatively'],
-      zip_safe=False)
+setup(
+    name="relatively",
+    version="1.0.3",
+    description="Abundance figure with absolute and relative abundances across a hierarchy",
+    url="https://github.com/brwnj/relatively",
+    author="Joe Brown",
+    author_email="brown@pm.me",
+    license="MIT",
+    packages=["relatively"],
+    install_requires=["numpy", "pandas>=0.23.1", "plotly>=3.0.0"],
+    zip_safe=False,
+)
