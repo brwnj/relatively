23dda8b9cd3681c87d0b04b772698e52f8b5ea1f
Joe Brown <brwnjm@gmail.com>
2018-09-24 10:12:38 -0700

    bumps version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="relatively",
-    version="1.0.3",
+    version="1.0.4",
     description="Abundance figure with absolute and relative abundances across a hierarchy",
     url="https://github.com/brwnj/relatively",
     author="Joe Brown",
