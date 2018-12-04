fd0597ea64fc7afd57d1295cf354b0cd88906b85
Joe Brown <brwnjm@gmail.com>
2018-12-04 16:19:54 -0700

    bumps version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="relatively",
-    version="1.0.4",
+    version="1.0.5",
     description="Abundance figure with absolute and relative abundances across a hierarchy",
     url="https://github.com/brwnj/relatively",
     author="Joe Brown",
-    author_email="brown@pm.me",
+    author_email="brwnjm@gmail.com",
     license="MIT",
     packages=["relatively"],
     install_requires=["numpy", "pandas>=0.23.1", "plotly>=3.0.0"],
